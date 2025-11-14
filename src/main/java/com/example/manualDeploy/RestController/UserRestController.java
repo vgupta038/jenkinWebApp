aebdf49001fb70f9088dfdb1589a84230ed72df9
Vishal Gupta <40133929+vgupta038@users.noreply.github.com>
2025-11-14 23:15:05 +0530

    Update UserRestController.java

--- src/main/java/com/example/manualDeploy/RestController/UserRestController.java
+++ src/main/java/com/example/manualDeploy/RestController/UserRestController.java
@@ -8,6 +8,6 @@ public class UserRestController {
 
 	@GetMapping("/test")
     public String testCall(){
-        return "welcome to tesgdfgfdgtfdfdf  dfsdsd   sprgboot project final commit!";
+        return "welcome to test the sprgboot project final commit!";
     }
 }
